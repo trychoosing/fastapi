092f7d6dbcc3907c9ec9a966a9a5c83dd3ede18d
trychoosing <trychoosing@gmail.com>
2025-09-19 16:50:08 +0800

    removed langchaincore dependency

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser 
+from fastapi.middleware.cors import CORSMiddleware 
 import os
 
 # Create an instance of FastAPI to serve as the main application.
